7f9f48675e7933f4e63ce8356d43114880d04c62
Gabriel Pariat <gabriel@pariatech.com>
2024-03-17 19:06:05 -0400

    fix??

--- create-appimage.sh
+++ create-appimage.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-linuxdeploy --desktop-file=urban-tycoons.desktop --appdir=AppDir --executable=urban-tycoons --icon-file=application-vnd.appimage.png
+linuxdeploy \
+    --desktop-file=urban-tycoons.desktop \
+    --appdir=AppDir \
+    --executable=urban-tycoons \
+    --icon-file=application-vnd.appimage.png
+
