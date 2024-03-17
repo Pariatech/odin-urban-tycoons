b99ec5bbf755a9542bf0805d4bf8e21522289cfa
Gabriel Pariat <gabriel@pariatech.com>
2024-03-17 11:39:53 -0400

    tweak to capture script

--- capture.sh
+++ capture.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+stop_ffmpeg() {
+    if [ -n "$pid" ]; then
+        echo "Stopping process..."
+        kill -TERM "$pid"
+    fi
+}
+
+trap stop_ffmpeg INT TERM
+
 get_property_value() {
     local property_name="$1"
     local output="$2"
@@ -12,4 +21,7 @@ y="$(get_property_value "Absolute upper-left Y" "$info")"
 width="$(get_property_value "Width" "$info")"
 height="$(get_property_value "Height" "$info")"
 
-ffmpeg -f x11grab -video_size "$width"x"$height" -framerate 30 -i :0.0+"$x","$y" -c:v libvpx-vp9 -y output.webm
+ffmpeg -f x11grab -video_size "$width"x"$height" -framerate 30 -i :0.0+"$x","$y" -c:v libvpx-vp9 -y output.webm &
+pid=$!
+
+wait "$pid"
