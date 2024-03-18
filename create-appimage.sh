b4e4837d79bb8bc8f3655233a295980f23e74970
Gabriel Pariat <gabriel@pariatech.com>
2024-03-17 20:47:03 -0400

    fix???

--- create-appimage.sh
+++ create-appimage.sh
@@ -6,3 +6,11 @@ linuxdeploy \
     --executable=urban-tycoons \
     --icon-file=application-vnd.appimage.png
 
+dependencies=$(ldd urban-tycoons | awk '{print $3}')
+
+for dep in $dependencies; do
+    cp "$dep" "AppDir/usr/lib"
+done 
+
+patchelf --set-rpath '$ORIGIN/../lib' "AppDir/usr/bin/urban-tycoons"
+
